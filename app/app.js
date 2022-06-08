fa675e6ee90cb822494ea31f760c018ec30e8b47
Kethlyn <kethlynsaraa@gmail.com>
2022-06-08 01:50:30 -0300

    fix: wrong imports

--- app/app.js
+++ app/app.js
@@ -6,3 +6,5 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(json());
+
+export default app;
--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-import app from './app/app.js'
+import app from './app/app.js';
+import db from './app/db.js';
 import dotenv from 'dotenv';
 dotenv.config();
+
+app.get('/signup/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const user = await db.query('SELECT * FROM users WHERE id = $1', [id]);
+        res.send(user.rows);
+    }
+    catch (e) {
+        res.sendStatus(500);
+        console.log(e);
+    }
+});
 
 app.listen(process.env.PORT, () => {
     console.log('Listening on port: ' + process.env.PORT);
